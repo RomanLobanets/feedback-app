febb4b43a61f5aaaf966562c1da88035fc5d6593
RomanLobanets <lobanets@gmail.com>
2020-08-25 21:04:12 -0400

    Updates

---
+++ precache-manifest.580aef724bb1a1c8e3e52366799e9780.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b10e62a750b9d2a93453b9cacd2b049",
+    "url": "/feedback-app/index.html"
+  },
+  {
+    "revision": "7c9d169b953eeb867adc",
+    "url": "/feedback-app/static/css/main.a6b28685.chunk.css"
+  },
+  {
+    "revision": "7c34356ff0d32477d4b5",
+    "url": "/feedback-app/static/js/2.34074e5f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/feedback-app/static/js/2.34074e5f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c9d169b953eeb867adc",
+    "url": "/feedback-app/static/js/main.82f09fb2.chunk.js"
+  },
+  {
+    "revision": "93ce5141ad420a27c22d",
+    "url": "/feedback-app/static/js/runtime-main.88958557.js"
+  }
+]);
